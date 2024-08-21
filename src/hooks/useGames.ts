c1ad8fdd7a8b395f4c4d19615cf9e7cc72430f69
Mariusz <mariusz.zalewski@komputronik.pl>
2024-08-21 16:43:42 +0200

    deal with strick mode cancel error

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react'
 import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
 
 interface Game {
   name: string;
@@ -14,10 +15,16 @@ const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
   useEffect(() => {
+    const controller = new AbortController();
+
     apiClient
       .get<fetchGamesRes>("/games")
       .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+    });
+    return () => controller.abort()
   });
 
   return {games, error}
