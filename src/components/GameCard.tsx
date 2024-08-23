e002650bec0726bae02c4370cb48c6c39b939853
Mariusz <mariusz.zalewski@komputronik.pl>
2024-08-23 10:34:08 +0200

    optimize images sizes

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import { PlatformList } from "./PlatformList";
 import { Critic } from "./Critic";
+import urlClip from "../services/url-clip";
 
 export interface Props {
   game: Game;
@@ -10,7 +11,7 @@ export interface Props {
 export const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={urlClip(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
